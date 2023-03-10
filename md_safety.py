e9c524ac97ed1ab1212ab5e2bfcaf321c300f99e
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2023-03-10 13:07:41 -0600

    Add files via upload

--- md_safety.py
+++ md_safety.py
@@ -23,44 +23,37 @@ class Safety(object):
         The subscribers should use the provided odom_callback and scan_callback as callback methods
         NOTE that the x component of the linear velocity in odom is the speed
         """
-        self.THRESHOLD = 0.4 
-        self.ranges = []
-        self.angles = []
-        self.angles_cos = []
-        self.speed = 0
-        self.angle_min = 0
-        self.angle_max = 0
-        self.angle_increment = 0
-        
-        self.initialized = False
+        self.THRESHOLD = 0.3 
+        self.speed = 0.75
+    
 
         rospy.Subscriber('scan', LaserScan, self.scan_callback)
-        rospy.Subscriber('odom', Odometry, self.odom_callback)
+       
 
-    def odom_callback(self, odom_msg):
-        # TODO: update current speed
-        self.speed = odom_msg.twist.twist.linear.x
+ 
 
     def scan_callback(self, scan_msg):
         # TODO: calculate TTC
         # TODO: publish brake message and publish controller bool
         self.ranges =np.array(scan_msg.ranges)
         # rospy.loginfo(np.min(self.ranges))
-        if self.initialized == False:
+        #if self.initialized == False:
         # read and save angle_min, angle_max, angle_increment
-            self.angle_min = scan_msg.angle_min
-            self.angle_max = scan_msg.angle_max
-            self.angle_increment = scan_msg.angle_increment
-            self.angles = np.linspace(self.angle_min,self.angle_max,len(self.ranges))
+        self.angle_min = scan_msg.angle_min
+        self.angle_max = scan_msg.angle_max
+        self.angle_increment = scan_msg.angle_increment
+        self.angles = np.linspace(self.angle_min,self.angle_max,len(self.ranges))
             # rospy.loginfo(len(self.ranges))
-            self.angles_cos = np.cos(self.angles)
+        self.angles_cos = np.cos(self.angles)
             # self.angles_cos = np.where(self.angles_cos>0, self.angles_cos, 0)
-            self.initialized = True
+        #self.initialized = True
         # rospy.loginfo(self.angles_cos)
-        self.calculate_TTC() 
-    def calculate_TTC(self):
+        #self.calculate_TTC() 
+    #def calculate_TTC(self):
         if self.speed < 0.1 and self.speed > -0.1:
             return 
+        drive_pub = rospy.Publisher("drive", AckermannDriveStamped, queue_size=10)
+        drive_msg=AckermannDriveStamped()
         subspeeds = self.angles_cos*self.speed
         # TTCs = np.divide(self.ranges, subspeeds,out=np.zeros_like(self.ranges), where=subspeeds!=0)
         TTCs = np.divide(self.ranges, subspeeds)
@@ -71,16 +64,12 @@ class Safety(object):
                 lower_than_threhold = True
                 break
         # rospy.loginfo(TTC)
-        if lower_than_threhold:
-            brake_bool_pub = rospy.Publisher("brake_bool", Bool, queue_size=10)
-            brake_bool_msg = Bool(data=True)
-            brake_bool_pub.publish(brake_bool_msg)
-
-            brake_pub = rospy.Publisher("vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size=10)
-            drive = AckermannDrive(speed=0)
-            brake_msg = AckermannDriveStamped(drive=drive)
-            brake_pub.publish(brake_msg)
+        if lower_than_threhold:  
             self.speed = 0
+            print(TTC)
+        
+        drive_msg.drive.speed=self.speed
+        drive_pub.publish(drive_msg)
             # rospy.loginfo("warning")
     
 def main():
